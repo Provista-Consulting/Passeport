81f3acfd9a6351d69472802b6f14728f910f55da
Fethi El Hassasna <e.fethi.c@gmail.com>
2015-05-04 15:17:02 +0100

    Update Spring lib to Swift 1.2

--- NovoMixPasseportSwift/Spring/Misc.swift
+++ NovoMixPasseportSwift/Spring/Misc.swift
@@ -23,7 +23,7 @@
 import UIKit
 
 public extension String {
-    public var length: Int { return countElements(self) }
+    public var length: Int { return count(self) }
 
     public func toURL() -> NSURL? {
         return NSURL(string: self)
@@ -72,7 +72,7 @@ public extension UIColor {
         let scanner = NSScanner(string: hex)
         var hexValue: CUnsignedLongLong = 0
         if scanner.scanHexLongLong(&hexValue) {
-            switch (countElements(hex)) {
+            switch (count(hex)) {
             case 3:
                 red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
                 green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
@@ -163,12 +163,12 @@ public func timeAgoSinceDate(date:NSDate, numericDates:Bool) -> String {
             return "1y"
         }
     } else if (components.month >= 2) {
-        return "\(components.month)"
+        return "\(components.month * 4)w"
     } else if (components.month >= 1){
         if (numericDates){
-            return "1M"
+            return "4w"
         } else {
-            return "1M"
+            return "4w"
         }
     } else if (components.weekOfYear >= 2) {
         return "\(components.weekOfYear)w"
