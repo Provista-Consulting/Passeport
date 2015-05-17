a3762faa6bcd9e1851b3b7b72bbe4162a6527f5f
Fethi El Hassasna <e.fethi.c@gmail.com>
2015-05-17 11:48:24 +0100

    Add NovoFine View controller

---
+++ NovoMixPasseportSwift/NovoFineViewController.swift
@@ -0,0 +1,77 @@
+//
+//  NovoFineViewController.swift
+//  NovoMixPasseportSwift
+//
+//  Created by Fethi El Hassasna on 5/14/15.
+//  Copyright (c) 2015 Fethi El Hassasna. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class NovoFineViewController: UIViewController {
+    
+    var titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 400, height: 44))
+    
+    var moviePlayer : MPMoviePlayerController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        createNavigationStyle()
+        
+        let path = NSBundle.mainBundle().pathForResource("novofine", ofType:"mp4")!
+        let url = NSURL.fileURLWithPath(path)
+        
+        moviePlayer = MPMoviePlayerController(contentURL: url)
+        
+        if let player = moviePlayer {
+            
+            player.view.frame = self.view.bounds
+            player.prepareToPlay()
+            player.scalingMode = .AspectFit
+            self.view.addSubview(player.view)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if let player = moviePlayer {
+            
+            player.stop()
+            player.view.removeFromSuperview()
+            moviePlayer = nil
+        }
+        
+    }
+    
+    func createNavigationStyle () {
+        
+        self.titleLabel.backgroundColor = UIColor.clearColor()
+        self.titleLabel.adjustsFontSizeToFitWidth = true
+        self.titleLabel.textColor = UIColor.blackColor()
+        self.titleLabel.textAlignment = .Center
+        self.titleLabel.font = UIFont.boldSystemFontOfSize(22.0)
+        self.titleLabel.text = "Guide d’utilisation du NovoFine®"
+        self.navigationItem.titleView = self.titleLabel
+        
+        // Customize navigation bar
+        self.navigationController?.navigationBar.translucent = true
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor.flatSkyBlueColorDark()
+        
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        self.titleLabel.textColor = UIColor.whiteColor()
+        
+        self.setNeedsStatusBarAppearanceUpdate()
+        
+    }
+
+}
