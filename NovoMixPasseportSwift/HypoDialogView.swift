dabcecfc377f49bf04b4422d6c7c7af1b16f3011
Fethi El Hassasna <e.fethi.c@gmail.com>
2015-05-17 11:57:50 +0100

    Create HypoDialogView view

---
+++ NovoMixPasseportSwift/HypoDialogView.swift
@@ -0,0 +1,21 @@
+//
+//  HypoDialogView.swift
+//  NovoMixPasseportSwift
+//
+//  Created by Fethi El Hassasna on 5/14/15.
+//  Copyright (c) 2015 Fethi El Hassasna. All rights reserved.
+//
+
+import UIKit
+
+class HypoDialogView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
