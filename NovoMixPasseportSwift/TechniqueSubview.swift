25a40742dbd9d88b2cb386199ea816adfb6116d5
Fethi El Hassasna <e.fethi.c@gmail.com>
2015-05-17 11:45:23 +0100

    Add NovoTwist subviews

---
+++ NovoMixPasseportSwift/TechniqueSubview.swift
@@ -0,0 +1,21 @@
+//
+//  TechniqueSubview.swift
+//  NovoMixPasseportSwift
+//
+//  Created by Fethi El Hassasna on 5/12/15.
+//  Copyright (c) 2015 Fethi El Hassasna. All rights reserved.
+//
+
+import UIKit
+
+class TechniqueSubview: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ NovoMixPasseportSwift/TypeDaiguillesSubview.swift
@@ -0,0 +1,21 @@
+//
+//  TypeDaiguillesSubview.swift
+//  NovoMixPasseportSwift
+//
+//  Created by Fethi El Hassasna on 5/12/15.
+//  Copyright (c) 2015 Fethi El Hassasna. All rights reserved.
+//
+
+import UIKit
+
+class TypeDaiguillesSubview: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
