7b92b99ba857e825831507c3f882d13011117167
Fethi El Hassasna <e.fethi.c@gmail.com>
2015-05-17 11:56:06 +0100

    Add Arrow button custom view

---
+++ NovoMixPasseportSwift/ArrowButton.swift
@@ -0,0 +1,24 @@
+//
+//  ArrowButton.swift
+//  NovoMixPasseportSwift
+//
+//  Created by Fethi El Hassasna on 5/14/15.
+//  Copyright (c) 2015 Fethi El Hassasna. All rights reserved.
+//
+
+import UIKit
+
+class ArrowButton: UIView {
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var button: TopButton!
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
