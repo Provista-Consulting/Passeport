58be6c07df17a6de2a28042a86c5824f870706b2
Fethi El Hassasna <e.fethi.c@gmail.com>
2015-05-18 11:58:45 +0100

    Add animated functional dialogs to Hypoglycemie view controller

--- NovoMixPasseportSwift/HypoDialogView.swift
+++ NovoMixPasseportSwift/HypoDialogView.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class HypoDialogView: UIView {
+class HypoDialogView: SpringView {
 
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var frontView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var frontIsHidden = true
     /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -17,5 +25,73 @@ class HypoDialogView: UIView {
         // Drawing code
     }
     */
+    
+    override var hidden : Bool{
+        
+        didSet {
+            
+            if !hidden {
+                NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "hideFrontView:", userInfo: nil, repeats: false)
+            } else {
+                NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "showFrontView:", userInfo: nil, repeats: false)
+            }
+        }
+    }
+    
+    func load () {
+        self.layer.shadowColor = UIColor.blackColor().CGColor
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowRadius = 10.0
+    }
 
+    @IBAction func click(sender: AnyObject) {
+        println(self.frontIsHidden)
+        
+        if self.frontIsHidden {
+           
+            self.frontIsHidden = false
+            self.showFrontView(nil)
+            
+        } else {
+            
+            self.frontIsHidden = true
+            self.hideFrontView(nil)
+            
+        }
+        
+    }
+    
+    func startAnimation () {
+        self.hidden = false
+        self.animation = "squeezeRight"
+        self.animate()
+    }
+    
+    func showFrontView (sender : AnyObject?) {
+        
+        UIView.animateWithDuration(0.5, animations: { () -> Void in
+            
+            self.button.transform = CGAffineTransformMakeRotation(0.0)
+            self.frontView.transform = CGAffineTransformMakeTranslation(0.0, 0.0)
+            
+            }) { (Bool) -> Void in
+                
+                // println("Finished!")
+        }
+        
+    }
+    
+    func hideFrontView (sender : AnyObject?) {
+        
+        UIView.animateWithDuration(0.5, animations: { () -> Void in
+            
+            self.button.transform = CGAffineTransformMakeRotation( CGFloat((M_PI * -1) / 2))
+            self.frontView.transform = CGAffineTransformMakeTranslation(0.0, -350.0)
+            
+            }) { (Bool) -> Void in
+                
+                // println("Finished!")
+        }
+        
+    }
 }
--- NovoMixPasseportSwift/HypoglycemieViewController.swift
+++ NovoMixPasseportSwift/HypoglycemieViewController.swift
@@ -12,6 +12,9 @@ class HypoglycemieViewController: UIViewController {
 
     var titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 400, height: 44))
     
+    let titles : [String] = ["Causes\nd’hypoglycémie".uppercaseString, "SYMPTÔMES DÉCELABLES\nPAR VOTRE ENTOURAGE", "SYMPTÔMES\nD’UNE HYPOGLYCÉMIE", "Que faire en cas\nd’hypoglycémie ".uppercaseString]
+    
+    let titlesBig : [String] = ["Causes d’hypoglycémie".uppercaseString, "SYMPTÔMES DÉCELABLES PAR VOTRE ENTOURAGE", "SYMPTÔMES D’UNE HYPOGLYCÉMIE", "Que faire en cas d’hypoglycémie ".uppercaseString]
     
     @IBOutlet weak var causesArrowButton: UIView!
     @IBOutlet weak var symptomesArrowButton: UIView!
@@ -23,6 +26,13 @@ class HypoglycemieViewController: UIViewController {
     var symptomes2Button : ArrowButton!
     var QueFaireButton : ArrowButton!
     
+    // Dialog stuff
+    
+    var bgDialogView : UIView = UIView()
+    var dialogView : HypoDialogView!
+    
+    var closeDialogButton : UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,39 +40,116 @@ class HypoglycemieViewController: UIViewController {
         
         // Causes button
         self.causesButton = NSBundle.mainBundle().loadNibNamed("ArrowButton", owner: self, options: nil)[0] as! ArrowButton
-        self.causesButton.labelTitle.text = "Causes\nd’hypoglycémie".uppercaseString
+        self.causesButton.labelTitle.text = titles[0]
         self.causesButton.button.tag = 0
         self.causesButton.button.addTarget(self, action: "showDialog:", forControlEvents: UIControlEvents.TouchUpInside)
         self.causesArrowButton.addSubview(self.causesButton)
         
         // symptomes button
         self.symptomesButton = NSBundle.mainBundle().loadNibNamed("ArrowButton", owner: self, options: nil)[0] as! ArrowButton
-        self.symptomesButton.labelTitle.text = "SYMPTÔMES DÉCELABLES\nPAR VOTRE ENTOURAGE"
+        self.symptomesButton.labelTitle.text = titles[1]
         self.symptomesButton.button.tag = 1
         self.symptomesButton.button.addTarget(self, action: "showDialog:", forControlEvents: UIControlEvents.TouchUpInside)
         self.symptomesArrowButton.addSubview(self.symptomesButton)
         
         // symptomes2 button
         self.symptomes2Button = NSBundle.mainBundle().loadNibNamed("ArrowButton", owner: self, options: nil)[0] as! ArrowButton
-        self.symptomes2Button.labelTitle.text = "SYMPTÔMES\nD’UNE HYPOGLYCÉMIE"
+        self.symptomes2Button.labelTitle.text = titles[2]
         self.symptomes2Button.button.tag = 2
         self.symptomes2Button.button.addTarget(self, action: "showDialog:", forControlEvents: UIControlEvents.TouchUpInside)
         self.symptomes2ArrowButton.addSubview(self.symptomes2Button)
         
         // QueFaireButton
         self.QueFaireButton = NSBundle.mainBundle().loadNibNamed("ArrowButton", owner: self, options: nil)[0] as! ArrowButton
-        self.QueFaireButton.labelTitle.text = "Que faire en cas\nd’hypoglycémie ".uppercaseString
+        self.QueFaireButton.labelTitle.text = titles[3]
         self.QueFaireButton.button.tag = 3
         self.QueFaireButton.button.addTarget(self, action: "showDialog:", forControlEvents: UIControlEvents.TouchUpInside)
         self.QueFaireArrowButton.addSubview(self.QueFaireButton)
         
         
         self.createNavigationStyle()
+        
+        // Dialog stuff
+        self.bgDialogView.frame = self.view.bounds
+        self.bgDialogView.backgroundColor = UIColor.clearColor()
+        var bgView = UIControl()
+        bgView.frame = self.view.bounds
+        bgView.backgroundColor = UIColor.whiteColor()
+        bgView.alpha = 0.9
+        bgView.addTarget(self, action: "closeDialog:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.bgDialogView.addSubview(bgView)
+        
+        
+        self.dialogView = NSBundle.mainBundle().loadNibNamed("HypoDialogView", owner: self, options: nil)[0] as! HypoDialogView
+        self.dialogView.load()
+        self.dialogView.center = self.bgDialogView.center
+        self.dialogView.backgroundColor = UIColor.clearColor()
+        self.bgDialogView.addSubview(self.dialogView)
+        //self.dialogView.hidden = true
+        
+        self.view.addSubview(self.bgDialogView)
+        self.bgDialogView.hidden = true
+        self.dialogView.hidden = true
+        
+        self.closeDialogButton = UIBarButtonItem(title: "Fermer", style: UIBarButtonItemStyle.Done, target: self, action: "closeDialog:")
+        
     }
     
     func showDialog (sender: AnyObject) {
         
+        
+        
         println(sender.tag)
+        
+        for subview in self.dialogView.scrollView.subviews {
+            
+            subview.removeFromSuperview()
+        }
+        
+        self.dialogView.titleLabel.text = titlesBig[sender.tag]
+        self.dialogView.scrollView.contentSize = CGSize(width: 563.0, height: 340.0)
+        
+        switch sender.tag {
+        case 0 :
+            let view1 = NSBundle.mainBundle().loadNibNamed("HypoDialogViewSubviews", owner: self, options: nil)[0] as! UIView
+            self.dialogView.scrollView.addSubview(view1)
+            break
+        case 1 :
+            let view1 = NSBundle.mainBundle().loadNibNamed("HypoDialogViewSubviews", owner: self, options: nil)[1] as! UIView
+            self.dialogView.scrollView.addSubview(view1)
+            break
+        case 2 :
+            let view1 = NSBundle.mainBundle().loadNibNamed("HypoDialogViewSubviews", owner: self, options: nil)[2] as! UIView
+            self.dialogView.scrollView.addSubview(view1)
+            
+            break
+        case 3 :
+            let view1 = NSBundle.mainBundle().loadNibNamed("HypoDialogViewSubviews", owner: self, options: nil)[3] as! UIView
+            var view2 = NSBundle.mainBundle().loadNibNamed("HypoDialogViewSubviews", owner: self, options: nil)[4] as! UIView
+            view2.frame.origin.y = 340.0
+            
+            self.dialogView.scrollView.addSubview(view1)
+            self.dialogView.scrollView.addSubview(view2)
+            
+            self.dialogView.scrollView.contentSize = CGSize(width: 563.0, height: CGFloat(340.0 * 2))
+            break
+        default :
+            break
+        }
+        
+        self.bgDialogView.hidden = false
+        self.dialogView.startAnimation()
+        
+        self.navigationItem.rightBarButtonItem = self.closeDialogButton
+        
+    }
+    
+    func closeDialog (sender : AnyObject) {
+        self.bgDialogView.hidden = true
+        self.dialogView.hidden = true
+        
+        self.navigationItem.rightBarButtonItem = nil
     }
     
     override func didReceiveMemoryWarning() {
